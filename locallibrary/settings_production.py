4a1eb8e40d5982b355a35ab57f5612af3b0cd6b0
Sergio1269529 <1269529@alu.murciaeduca.es>
2024-02-06 18:23:05 +0100

    Variables

--- locallibrary/settings_production.py
+++ locallibrary/settings_production.py
@@ -1,15 +1,24 @@
-from decouple import config
+from decouple import Config
+
+# Configura la clase Config con el archivo .env
+config = Config()
+
+# Define las variables de entorno necesarias
+POSTGRES_DATABASE = config('POSTGRES_DATABASE')
+POSTGRES_USER = config('POSTGRES_USER')
+POSTGRES_PASSWORD = config('POSTGRES_PASSWORD')
+POSTGRES_HOST = config('POSTGRES_HOST')
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': config('POSTGRES_HOST'),
-        'PORT': 5432,
-        'NAME': config('POSTGRES_DATABASE'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DATABASE,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+        'PORT': '5432',  
     }
 }
